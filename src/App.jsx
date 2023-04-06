482693ed7a3410966b7890c8b22581d2bf28154d
Rachael McIlhagga <rmcilhagga@tvo.org>
2023-04-06 16:42:55 -0400

    adding gltf model

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,25 @@
-import Polyhedron from './Polyhedron'
-import * as THREE from 'three'
+import { Stats, OrbitControls, Circle } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
-import { Stats, OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
-import Floor from './Floor'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
-function Lights() {
-  const ambientCtl = useControls('Ambient Light', {
-    visible: false,
-    intensity: {
-      value: 1.0,
-      min: 0,
-      max: 1.0,
-      step: 0.1,
-    },
-  })
-
-  const directionalCtl = useControls('Directional Light', {
-    visible: true,
-    position: {
-      x: 3.3,
-      y: 1.0,
-      z: 4.4,
-    },
-    castShadow: true,
-  })
-
-  const pointCtl = useControls('Point Light', {
-    visible: false,
-    position: {
-      x: 2,
-      y: 0,
-      z: 0,
-    },
-    castShadow: true,
-  })
-
-  const spotCtl = useControls('Spot Light', {
-    visible: false,
-    position: {
-      x: 30,
-      y: 2.5,
-      z: 1,
-    },
-    castShadow: true,
-  })
-
-  return (
-    <>
-      <ambientLight
-        visible={ambientCtl.visible}
-        intensity={ambientCtl.intensity}
-      />
-      <directionalLight
-        visible={directionalCtl.visible}
-        position={[
-          directionalCtl.position.x,
-          directionalCtl.position.y,
-          directionalCtl.position.z,
-        ]}
-        castShadow={directionalCtl.castShadow}
-      />
-      <pointLight
-        visible={pointCtl.visible}
-        position={[
-          pointCtl.position.x,
-          pointCtl.position.y,
-          pointCtl.position.z,
-        ]}
-        castShadow={pointCtl.castShadow}
-      />
-      <spotLight
-        visible={spotCtl.visible}
-        position={[spotCtl.position.x, spotCtl.position.y, spotCtl.position.z]}
-        castShadow={spotCtl.castShadow}
-      />
-    </>
-  )
-}
 export default function App() {
-  const texture = useLoader(THREE.TextureLoader, './img/grid.png')
+  const gltf = useLoader(GLTFLoader, './models/monkey.glb')
 
   return (
-    <Canvas camera={{ position: [4, 4, 1.5] }} shadows>
-      <Lights />
-      <Polyhedron
-        name="meshBasicMaterial"
-        position={[-3, 1, 0]}
-        material={new THREE.MeshBasicMaterial({ map: texture })}
+    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+      <directionalLight position={[2.3, 1.0, 3.4]} castShadow />
+      <primitive
+        object={gltf.scene}
+        position={[0, 1, 0]}
+        children-0-castShadow
       />
-      <Polyhedron
-        name="meshNormalMaterial"
-        position={[-1, 1, 0]}
-        material={new THREE.MeshNormalMaterial({ flatShading: true,map: texture,
-        })}
-      />
-      <Polyhedron
-        name="meshPhongMaterial"
-        position={[1, 1, 0]}
-        material={
-          new THREE.MeshPhongMaterial({ color: 'lime', flatShading: true,            map: texture,
-        })
-        }
-      />
-      <Polyhedron
-        name="meshStandardMaterial"
-        position={[3, 1, 0]}
-        material={
-          new THREE.MeshStandardMaterial({
-            color: 0xff0033,
-            flatShading: true,
-            map: texture,
-
-          })
-        }
-      />
-      <Floor />
-      <OrbitControls target={[2, 2, 0]} />
+      {/* this is the floor */}
+      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
+        <meshStandardMaterial />
+      </Circle>
+      <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
       <Stats />
     </Canvas>
   )
-}
+} 
