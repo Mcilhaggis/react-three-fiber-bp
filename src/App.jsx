a5965f827adab2f58aa35b83682f26bd7e0f1065
Rachael McIlhagga <rmcilhagga@tvo.org>
2023-04-19 17:04:55 -0400

    section 24

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,87 @@
-import { Stats, OrbitControls, Sphere } from '@react-three/drei'
+import { Stats, OrbitControls, Environment, ContactShadows } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { Environment } from '@react-three/drei'
+import { Leva, useControls } from 'leva'
+
+function Env() {
+  const { height, radius, scale } = useControls('Ground', {
+    height: { value: 10, min: 0, max: 100, step: 1 },
+    radius: { value: 115, min: 0, max: 1000, step: 1 },
+    scale: { value: 100, min: 0, max: 1000, step: 1 },
+  })
+  return (
+    <Environment ground={{
+      height: height,
+      radius: radius,
+      scale: scale
+    }}
+      files="./img/belfast_sunset_4k.hdr" background />
+  )
+}
+
+function Model() {
+  const { scene } = useLoader(GLTFLoader, './models/scene.glb')
+
+  const {
+    x,
+    y,
+    z,
+    visible,
+    color,
+    metalness,
+    roughness,
+    clearcoat,
+    clearcoatRoughness,
+    transmission,
+    ior,
+    thickness,
+  } = useControls('Suzanne', {
+    x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    visible: true,
+    color: { value: '#ffbc85' },
+    metalness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    roughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    clearcoat: { value: 1, min: 0, max: 1.0, step: 0.01 },
+    clearcoatRoughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    transmission: { value: 1.0, min: 0, max: 1.0, step: 0.01 },
+    ior: { value: 1.74, min: 1, max: 5, step: 0.01 },
+    thickness: { value: 3.12, min: 0, max: 5, step: 0.01 },
+  })
+  return (
+    <primitive object={scene}
+      children-0-rotation={[x, y, z]}
+      children-0-visible={visible}
+      children-0-material-color={color}
+      children-0-material-metalness={metalness}
+      children-0-material-roughness={roughness}
+      children-0-material-clearcoat={clearcoat}
+      children-0-material-clearcoatRoughness={clearcoatRoughness}
+      children-0-material-transmission={transmission}
+      children-0-material-ior={ior}
+      children-0-material-thickness={thickness}
+    />
+
+  )
+}
 
 export default function App() {
-  const gltf = useLoader(GLTFLoader, './models/monkey.glb')
 
   return (
-    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
-      {/* <directionalLight position={[2.3, 1.0, 3.4]} castShadow /> */}
-      <primitive
-        object={gltf.scene}
-        position={[0, 1, 0]}
-        children-0-castShadow
-      />
-      <directionalLight position={[3.3, 8.5, 4.4]} intensity={4}/>
-        {/* adding in sphere can help to decipher where the light should be positioned to most acurately represent the HDR light source */}
-     
-      {/* A grat alternative to using lighting  */}
-      <Environment files='./img/brown_photostudio_02_4k.hdr' background blur={0.06} />
-      {/* this is the floor */}
-      {/* <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-        <meshStandardMaterial />
-      </Circle> */}
-      <OrbitControls target={[0, 1, 0]}autoRotate />
-      <axesHelper args={[5]} />
-      <Stats />
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [-8, 5, 8] }}>
+        {/* You can use a leva control to figure out these values */}
+        <Env />
+        {/* <directionalLight position={[3.3, 1.0, 4.4]} intensity={4} /> */}
+        <Model />
+        <OrbitControls target={[0, 1, 0]} maxPolarAngle={Math.PI / 2} />
+        <ContactShadows scale={150} position={[0.33, -0.33, 0.33]} opacity={1.5}/>
+        <axesHelper args={[5]} />
+        <Stats />
+      </Canvas>
+      <Leva collapsed />
+    </>
+
   )
-} 
+}
