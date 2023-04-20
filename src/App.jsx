942e05eabedb524b1acccf7316d2c546bcb1e616
Rachael McIlhagga <rmcilhagga@tvo.org>
2023-04-20 14:33:59 -0400

    adding annotations to models in blender and displaying in the scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,73 @@
+import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Stats, OrbitControls, Environment, useGLTF } from '@react-three/drei'
+import {
+  useGLTF,
+  OrbitControls,
+  Environment,
+  Stats,
+  Html,
+} from '@react-three/drei'
 import { useControls } from 'leva'
-
-const Models = [
-  { title: 'Box', url: './models/box.glb' },
-  { title: 'Plant 1', url: './models/plant-1.glb' },
-  { title: 'Plant 2', url: './models/plant-2.glb' },
-]
+import Models from './models'
 
 function Model({ url }) {
   const { scene } = useGLTF(url)
-  return <primitive object={scene} />
+  // Creating a cache to store the annotations
+  const [cache, setCache] = useState({})
+
+  if (!cache[url]) {
+    const annotations = []
+
+    scene.traverse((o) => {
+      if (o.userData.prop) {
+        annotations.push(
+          <Html
+            key={o.uuid}
+            position={[o.position.x, o.position.y, o.position.z]}
+            // transform
+            distanceFactor={0.25}
+            // occlude would hide any of the values that we placed inside of the mesh
+            // occlude
+
+          >
+            {/* Is the value of the string applied in Blender */}
+            <div className="annotation">{o.userData.prop}</div>
+          </Html>
+        )
+      }
+    })
+
+    console.log('Caching JSX for url ' + url)
+    // This code with allow us to build up the content stored in the scene, it will append new values
+    setCache({
+      ...cache,
+      [url]: <primitive object={scene}>{annotations}</primitive>,
+    })
+  }
+  return cache[url]
 }
 
 export default function App() {
-  const { title } = useControls({
-    title: {
-      options: Models.map(({ title }) => title),
+  const { model } = useControls({
+    model: {
+      value: 'box',
+      options: Object.keys(Models),
     },
   })
 
   return (
     <>
-      <Canvas camera={{ position: [0.5, 0, -0.2], near: 0.025 }}>
+      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
         <Environment files="./img/abandoned_greenhouse_4k.hdr" background />
         <group>
-          <Model
-            url={Models[Models.findIndex((m) => m.title === title)].url}
-          />
+          <Model url={Models[model]} />
         </group>
         <OrbitControls  />
         <Stats />
       </Canvas>
-      <span id="info">The {title} is selected.</span>
+      <span id="info">
+        The {model.replace(/([A-Z])/g, ' $1').toLowerCase()} is selected.
+      </span>
     </>
   )
 }
